203ad45fcffb587a101f8f053d8e4d785efdecfa
=vicenc.fernandez <=vicenc.fernandez@gmail.com>
2025-08-06 19:00:00 +0200

    con pos

--- TeachingManagement/settings.py
+++ TeachingManagement/settings.py
@@ -91,11 +91,10 @@ WSGI_APPLICATION = 'TeachingManagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 
